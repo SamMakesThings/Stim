a632c536357c1edacb66ce449faf364787112913
austin <aguspdc@gmail.com>
2023-10-07 22:07:53 -0700

    Working server start

--- main/app.py
+++ main/app.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
 import os
+from enum import Enum
+from fastapi import FastAPI
 from dotenv import load_dotenv
 from supabase import create_client, Client
 from agent_protocol import Agent, Step, Task
@@ -11,6 +12,41 @@ key: str = os.environ.get("DATABASE_KEY")
 supabase: Client = create_client(url, key)
 
 app = FastAPI()
+
+
+class StepTypes(str, Enum):
+    PROCESS = "process"  # Process Stimulus
+    SEND = "send"  # Send Message
+
+
+# Does this make sense? Think so...
+
+
+async def _process_stimuli(step: Step) -> Step:
+    # Process the stimulus
+    # NOTE: Chiung-Yi add code here
+    pass
+
+
+async def _message_user(step: Step) -> Step:
+    # Send the message to the user
+    pass
+
+
+async def task_handler(task: Task) -> None:
+    if not task.input:
+        raise Exception("No task prompt")
+    await Agent.db.create_step(task.task_id, StepTypes.PROCESS)
+
+
+async def step_handler(step: Step):
+    task = await Agent.db.get_task(step.task_id)
+    if step.name == StepTypes.PROCESS:
+        return await _process_stimuli(step)
+    elif step.name == StepTypes.SEND:
+        return await _message_user(task, step)
+    else:
+        return await _process_stimuli(task, step)
 
 
 # Insert Agent Protocol stuff here
