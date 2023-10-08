3961a2eadda9453fca68f9a507d6b5a27e08e315
austin <aguspdc@gmail.com>
2023-10-08 04:31:48 -0700

    in-progress

--- main/relevance_agent.py
+++ main/relevance_agent.py
@@ -1,39 +1,95 @@
 import openai
 import os
 from supabase_db import supabase  # ensure you have imported your Supabase wrapper
+import logging
 
-class Relevance:
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-    def __init__(self, db_wrapper):
-        self.openai_api_key = os.environ.get('OPENAI_API_KEY')
+
+class RelevanceAgent:
+    def __init__(self):
+        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
         if not self.openai_api_key:
             raise ValueError("OpenAI API key not found in environment variables")
         openai.api_key = self.openai_api_key
         self.topic_cache = set()
 
-    def processRelevance(self, message, message_id):
-        response = openai.Completion.create(prompt=f"Extract topic and relevance score (0 be lowest, 1 be highest) from below article. Output in the json format. Only pick the topic with highest relevance: {message}", max_tokens=50)
-        
+    def batch_stimulus_into_topic(self, stimulus):
+        topics = supabase.table("topic_batches").select("*").execute().data
+        topics_str = "\n".join([topic["topic"] for topic in topics])
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": f"""
+                    You are a topic identification system. You receive messages that need to
+                    be categorized into topics. If no existing topic clearly represents the message
+                    content, create a new topic.
+                    Respond with only the string of the topic, no quotes, no fluff, nothing other than
+                    the topic itself.
+                    
+                    Existing Topics:
+                    {topics_str}
+                    """,
+                    },
+                    {
+                        "role": "user",
+                        "content": f"{stimulus['sender']}: {stimulus['content']}",
+                    },
+                ],
+            )
+            print(f"topic: {response}")
+
+        except Exception as e:
+            logger.error(e)
+
         # Split and extraction process might be more complex based on the actual OpenAI response
-        topic, relevance = response.choices[0].text.split(":")
-        print(f"message_id: {message_id} topic: {topic} relevance: {relevance}")
+        topic = response
+        logger.info(f"topic: {topic}")
 
-        self.store_in_db(topic.strip(), message_id, relevance.strip())
+        # self.upsert_topic_batches(topic.strip(), stimulus)
 
-    def store_in_db(self, topic, message_id, relevance):
+    def upsert_topic_batches(self, topic, message_id, relevance):
         if topic not in self.topic_cache:
-            data, count = supabase.table('topics').upsert({'topic': topic, 'message_ids': [message_id], 'relevance': relevance}).execute()
+            data, count = (
+                supabase.table("topics")
+                .upsert(
+                    {
+                        "topic": topic,
+                        "message_ids": [message_id],
+                        "relevance": relevance,
+                    }
+                )
+                .execute()
+            )
             self.topic_cache.add(topic)
         else:
-            data, count = supabase.table('topics').select('message_ids').eq('topic', topic).execute()
-            if data and 'message_ids' in data[0]:
-                message_ids = data[0]['message_ids']
+            data, count = (
+                supabase.table("topics")
+                .select("message_ids")
+                .eq("topic", topic)
+                .execute()
+            )
+            if data and "message_ids" in data[0]:
+                message_ids = data[0]["message_ids"]
                 message_ids.append(message_id)
-                data, count = supabase.table('topics').upsert({'topic': topic, 'message_ids': message_ids}).execute()
+                data, count = (
+                    supabase.table("topics")
+                    .upsert({"topic": topic, "message_ids": message_ids})
+                    .execute()
+                )
+
 
 # Example Usage
-# db_wrapper is defined for the purpose of example, but not used in the code. You might want to integrate it properly.
-db_wrapper = PostgresWrapper(db_connection_string="YOUR_DB_CONNECTION_STRING")
-relevance = Relevance(db_wrapper)
-relevance.processRelevance("Your message here", "message_id_123")
-db_wrapper.close()  # Ensure your wrapper has a close method
+relevance = RelevanceAgent()
+relevance.batch_stimulus_into_topic(
+    {
+        "sender": "Alex",
+        "content": "Who is joining for coffee at 4?",
+        "message_id": 1,
+        "relevance": 0.5,
+    }
+)
---
+++ main/rtdb.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+# DB Subscription Stuff
+from realtime.connection import Socket  # noqa: E402
+
+load_dotenv()
+
+
+def callback1(payload):
+    print("Callback 1: ", payload)
+
+
+if __name__ == "__main__":
+    URL = f"wss://{os.environ.get('DATABASE_ID')}.supabase.co/realtime/v1/websocket?apikey={os.environ.get('DATABASE_KEY')}&vsn=1.0.0"  # noqa: E501
+    s = Socket(URL)
+    s.connect()
+
+    channel_1 = s.set_channel("realtime:public:topic_batches")
+    channel_1.join().on("*", callback1)
+    s.listen()
