8232d03fb683ee389902a5bd8348e39bf0e445fb
austin <aguspdc@gmail.com>
2023-10-07 21:01:22 -0700

    Supabase stuff

--- main/app.py
+++ main/app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from agent_protocol import Agent, Step, Task
+
+load_dotenv()
+
+url: str = os.environ.get("DATABASE_URL")
+key: str = os.environ.get("DATABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
 
 # Insert Agent Protocol stuff here
-@app.post("/receive_stim_message")
+@app.post("/stimulus/")
 def receive_stim_message():
     return {"ok": True}
 
